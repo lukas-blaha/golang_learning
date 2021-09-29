7302b6b697b1b55d71f3f730c8685595bde95ae8
Lukas Blaha <lukas.blaha@seznam.cz>
2021-09-29 10:31:45 +0200

    Add all the code after day three

--- functions/functions1.go
+++ functions/functions1.go
@@ -7,10 +7,31 @@ import (
 
 func main() {
     foo()
+    bar("James")
+    s1 := woo("Moneypenny")
+    fmt.Println(s1)
+    x, y := mouse("Ian", "Fleming")
+    fmt.Println(x)
+    fmt.Println(y)
 }
 
 // func (r receiver) identifier(params) (return(s)) { ... }
 
 func foo() {
     fmt.Println("hello from foo")
+}
+
+// everything in GO is PASS BY VALUE
+func bar(s string) {
+    fmt.Println("hello,", s)
+}
+
+func woo(s string) string {
+    return fmt.Sprint("Hello from woo, ", s)
+}
+
+func mouse(fn, ln string) (string, bool) {
+    a := fmt.Sprint(fn, " ", ln, `, says "hello"`)
+    b := false
+    return a, b
 }
---
+++ functions/functions2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+func main() {
+    // xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+    // x := sum(xi...)
+    x := sum()
+    fmt.Println(x)
+}
+
+func sum(x ...int) int {
+    fmt.Println(x)
+
+    sum := 0
+    for _, v := range x {
+        sum += v
+    }
+    return sum
+}
